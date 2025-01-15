1d3cfa133a5e452ad36c17814838225058adfa97
Abdullah <abdullahahmedsyed65@gmail.com>
2025-01-15 07:32:08 -0500

    fix tail

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,15 @@ import { useSearchParams,useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  const searchParams = useSearchParams()
-  const name = searchParams.get('name')
-  const password = searchParams.get('password')
   const router = useRouter()
 
   useEffect(() => {
-    router.push('/patient')
+    router.push('/login')
   }, [])
   
   return (
     <>
-      Name: {name}
-      Password: {password}
+      NULL
     </>
   );
 }
